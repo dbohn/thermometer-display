6caf7dab1d759444830f2f6459bb25950c6f7382
David Bohn <davbohn@googlemail.com>
2020-06-21 22:15:50 +0200

    Fix buttons 13 and 19

--- epaper/src/gpio.h
+++ epaper/src/gpio.h
@@ -1,3 +1,5 @@
 #define FFI_LIB "libwiringPi.so"
 
 int wiringPiISR(int pin, int edgeType,  void (*function)(void));
+void pinMode(int pin, int mode);
+void pullUpDnControl(int pin, int pud);
--- src/Display/Buttons.php
+++ src/Display/Buttons.php
@@ -14,12 +14,21 @@ class Buttons
 
     public const KEY1 = 5;
     public const KEY2 = 6;
-    public const KEY3 = 13; // Not working
-    public const KEY4 = 19; // Not working
+    public const KEY3 = 13;
+    public const KEY4 = 19;
 
     public const EDGE_FALLING = 1;
     public const EDGE_RISING = 2;
     public const EDGE_BOTH = 3;
+
+    protected const MODE_INPUT = 0;
+    protected const MODE_OUTPUT = 1;
+
+    protected const PUD_OFF = 0;
+    protected const PUD_DOWN = 1;
+    protected const PUD_UP = 2;
+
+    protected const DEBOUNCE_INTERVAL = 0.3;
 
     protected $locked = [
         self::KEY1 => false,
@@ -41,20 +50,19 @@ class Buttons
      */
     public function register($handler, $button = self::KEY1, $edge = self::EDGE_FALLING)
     {
+        $this->ffi->pinMode($button, self::MODE_INPUT);
+        $this->ffi->pullUpDnControl($button, self::PUD_UP);
         $this->ffi->wiringPiISR($button, $edge, fn () => $this->debounce($button, $handler));
     }
 
     protected function debounce($button, $handler)
     {
-        if ($this->locked[$button]) {
+        $now = microtime(true);
+        if ($this->locked[$button] !== false && $now < ($this->locked[$button] + self::DEBOUNCE_INTERVAL) ) {
             return;
         }
 
-        $this->locked[$button] = true;
-        $this->loop->addTimer(0.1, function () use ($button) {
-            echo "Unlock";
-            $this->locked[$button] = false;
-        });
+        $this->locked[$button] = $now;
 
         $handler();
     }
